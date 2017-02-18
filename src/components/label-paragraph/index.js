cac533a349c1bb9a00c77c9a9730c313a3077dce
Mauricio Kruijer <mauricio@leadsocial.com>
2017-02-18 17:44:13 +0100

    Label paragraph component

---
+++ src/components/label-paragraph/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class LabelParagraph extends React.Component {
+  render() {
+    const bgColor = {
+      backgroundColor: 'red'
+    };
+    return (
+      <section className="section group">
+        <div className="label-paragraph-col span_1_of_4" style={bgColor}>
+          {this.props.label}
+        </div>
+        <div className="label-paragraph-col span_3_of_4" style={bgColor}>
+          {this.props.children}
+        </div>
+      </section>
+    );
+  }
+}
