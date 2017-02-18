cddfaa6ae64ee63d48c19954bf3ab52b98c1332f
Mauricio Kruijer <mauricio@leadsocial.com>
2017-02-18 16:38:52 +0100

    Grid system

--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,47 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>It Works!</h1>
+        <h1>It Works! boom</h1>
         <p>This React project just works including <span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
+        <div className="section group">
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+          <div className="col span_1_of_12">
+            1 of 12
+          </div>
+        </div>
       </div>
     )
   }
