6ac7ee1f2f8a183bc24c724a9eb5539ed24e2baf
Mauricio Kruijer <mauricio@leadsocial.com>
2017-02-19 16:07:42 +0100

    Second page test

--- src/app.jsx
+++ src/app.jsx
@@ -1,53 +1,37 @@
 import React from 'react';
 import Header from './components/header';
 import Contact from './components/contact';
+import LabelParagraph from './components/label-paragraph';
 
 export default class App extends React.Component {
   render() {
+    const overRideStyle = {
+      float: 'none',
+      margin: '240px auto 0'
+    };
     return (
       <div>
         <Header />
         <Contact />
-        <h1>It Works! boom</h1>
-        <p>This React project just works including <span className="redBg">module</span> local styles.</p>
-        <p>Enjoy!</p>
+
         <div className="section group">
-          <div className="col span_1_of_12">
-            1 of 12
+          <div className="col span_8_of_12" style={overRideStyle}>
+            <LabelParagraph label="over">
+              The Odd Visual Stuff denkt, ontwerpt en creëert interdisiplinair.
+              Dat betekent dat je niet klaar bent met alleen een logo of website. Maar het begint allemaal concept. Een groot netwerk van creative collaga’s maakt het mogelijk om zo divers Design te maken.
+            </LabelParagraph>
           </div>
-          <div className="col span_1_of_12">
-            1 of 12
+
+          <div className="col span_8_of_12" style={overRideStyle}>
+            <LabelParagraph label="services" style={overRideStyle}>
+              Verbazen en laten zien wat er mogelijk is. Dat is het doel van Creative Direction. The Odd Visual Stuff helpt mensen en merken de volgende stap te maken Een duidelijk idee en motivatie is net zo belangerijk.
+            </LabelParagraph>
           </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
-          <div className="col span_1_of_12">
-            1 of 12
-          </div>
+        </div>
+
+        <div className="page" style={{height: '100vh', widht: '100%', position: 'relative', background: 'transparent'}}>
+          <Header />
+          <h1 style={{position: 'absolute', bottom: '10px', right: '10px'}}>(inter)planetary Creative Direction services</h1>
         </div>
       </div>
     )
