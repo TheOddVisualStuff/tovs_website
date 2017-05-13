68ffa1088e5936d1c23897459af77389e9475675
Mauricio Kruijer <mauricio@leadsocial.com>
2017-05-13 14:10:24 +0200

    H1 uppercase

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="header">
-        <h1>theoddvisualstuff</h1>
+        <h1 className="fs-24px tt-uppercase fw-normal">theoddvisualstuff</h1>
         <a href="#home">Terug naar Aarde</a>
       </header>
     );
