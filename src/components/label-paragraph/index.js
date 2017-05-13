e85bbf47acb4625d519327fb8f11ac95394bbd3e
Mauricio Kruijer <mauricio@leadsocial.com>
2017-05-13 14:10:09 +0200

    Label spacing

--- src/components/label-paragraph/index.js
+++ src/components/label-paragraph/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 export default class LabelParagraph extends React.Component {
   render() {
-    const bgColor = {
-      // backgroundColor: 'red'
+    const childrenStyle = {
+      fontSize: '23px'
+    };
+    const labelStyle = {
+      fontFamily: 'belwe',
+      fontSize: '13px'
     };
     return (
       <section className="section group">
-        <div className="label-paragraph-col span_1_of_4" style={bgColor}>
-          {this.props.label}
+        <div className="label-paragraph-col span_1_of_4" style={labelStyle}>
+          <div style={{ textAlign: 'right', paddingRight: '10px', paddingTop: '5px' }}>
+            {this.props.label}
+          </div>
         </div>
-        <div className="label-paragraph-col span_3_of_4" style={bgColor}>
+        <div className="label-paragraph-col span_3_of_4" style={childrenStyle}>
           {this.props.children}
         </div>
       </section>
