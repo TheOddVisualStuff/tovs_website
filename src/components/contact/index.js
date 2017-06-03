3c286725763bd851b7db691f0e734b82d4024875
Mauricio Kruijer <mauricio@leadsocial.com>
2017-06-03 18:01:50 +0200

    Added def about/contact texts
    Todo, think of a way to make the grid more flexable 😅 or the labeld paragraphs 😜

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -46,6 +46,70 @@ export default class Contact extends React.Component {
             Meld je aan voor de Nieuwsbrief
           </div>
         </section>
+
+        <section className="max-width-970 section group" style={{ margin: '0 auto', paddingTop: '105px' }}>
+          <div className="col fs-23px f-paragraph" style={{ width: '21%'}}>
+
+            <section className="section group">
+              <div className="col span_4_of_12 ta-right">
+                <div style={ Object.assign({}, labelStyle, {paddingTop: '6px'}) }>
+                  Snel
+                </div>
+              </div>
+              <div className="col0 span_8_of_12 fs-23px f-paragraph">
+                +3106123456
+              </div>
+            </section>
+
+            <section className="section group">
+              <div className="col span_4_of_12 ta-right">
+                <div style={ Object.assign({}, labelStyle, {paddingTop: '6px'}) }>
+                  Traag
+                </div>
+              </div>
+              <div className="col0 span_8_of_12 fs-23px f-paragraph">
+                orphe@tovs.nla
+              </div>
+            </section>
+
+          </div>
+
+          <div className="col0 span_2_of_12 ta-right">
+            <div style={ labelStyle }>
+              Ontmoet
+            </div>
+          </div>
+          <div className="col0 span_3_of_12 fs-23px f-paragraph">
+            Groene Hilledijk <br />
+            3073 AG Rotterdam <br />
+            Nederland
+          </div>
+
+
+          <div className="col0 fs-23px f-paragraph" style={{ width: '25%'}}>
+            <section className="section group">
+              <div className="col span_4_of_12 ta-right">
+                <div style={ Object.assign({}, labelStyle, {paddingTop: '6px'}) }>
+                  KvK
+                </div>
+              </div>
+              <div className="col0 span_8_of_12 fs-23px f-paragraph">
+                60376651
+              </div>
+            </section>
+            <section className="section group">
+              <div className="col span_4_of_12 ta-right">
+                <div style={ Object.assign({}, labelStyle, {paddingTop: '6px'}) }>
+                  BTW
+                </div>
+              </div>
+              <div className="col0 span_8_of_12 fs-23px f-paragraph">
+                NL117370101B01
+              </div>
+            </section>
+          </div>
+
+        </section>
       </div>
     );
   }
