2263443dc9441ac53fc805666a5d2ba5a75f62e1
Mauricio Kruijer <mauricio@leadsocial.com>
2017-06-28 00:20:03 +0200

    Component refactor plus scss

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import cn from 'classnames';
 
-export default class Header extends React.Component {
-  render() {
-    const h1Style = {
-      margin: 0,
-      float: 'left'
-    };
+const h1Style = {
+  margin: 0,
+  float: 'left'
+};
 
-    const aStyle = {
-      float: 'right',
-      fontSize: '15px',
-      fontFamily: 'belwe'
-    };
+const aStyle = {
+  float: 'right',
+  fontSize: '15px',
+  fontFamily: 'belwe'
+};
 
-    const isLargeClass = this.props.isLarge && 'large' ? 'top large' : 'small';
+const Header = ({isLarge, linkSrc, linkName}) => {
+  const isLargeClass = isLarge && 'large' ? 'Header__top Header__large' : 'Header__small';
+  return (
+    <header className={cn('Header', isLargeClass)}>
+      <div className="margin-0-auto max-width-1200">
+        <h1 className="fs-25px tt-uppercase f-tovs" style={ h1Style }>theoddvisualstuff</h1>
+        <a href={linkSrc} style={ aStyle }>{linkName}</a>
+      </div>
+    </header>
+  )
+};
 
-    return (
-      <header className={cn('Header', isLargeClass)}>
-        <div className="margin-0-auto max-width-1200">
-          <h1 className="fs-25px tt-uppercase f-tovs" style={ h1Style }>theoddvisualstuff</h1>
-          <a href={this.props.linkSrc} style={ aStyle }>{this.props.linkName}</a>
-        </div>
-      </header>
-    );
-  }
-}
+export default Header;
